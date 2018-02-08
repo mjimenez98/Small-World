00744eb558f609aeb726b7ff4a700476644e5759
Miguel Jimenez <migueljimenez@Miguels-MBP.local>
2018-02-08 01:29:56 -0500

    functions added

---
+++ Player/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Miguel Jimenez on 2/4/18.
+//
+
+#include "Player.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+};
---
+++ SummarySheet/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Miguel Jimenez on 2/7/18.
+//
+
+#include "../LostTribeToken/LostTribeToken.h"
+#include "../MatchingRaceToken/MatchingRaceToken.h"
+#include "../SpecialPowerBadge/SpecialPowerBadge.h"
+#include "../VictoryCoin/VictoryCoin.h"
+
+int main() {
+
+
+    return 0;
+}
+
